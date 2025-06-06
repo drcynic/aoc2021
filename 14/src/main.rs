10e5f2fb5738500760432269771213f479dd12e1
cynic <daniel@szymanski.cc>
2025-06-06 17:24:45 +0200

    Add day 14 part 2

--- 14/src/main.rs
+++ 14/src/main.rs
@@ -5,7 +5,7 @@ use std::iter::*;
 use itertools::Itertools;
 
 fn main() {
-    let filename = "input2.txt";
+    let filename = "input1.txt";
     let input = fs::read_to_string(filename).unwrap();
     let (start, instr) = input.split_once("\n\n").unwrap();
     println!("start: {:?}", start);
@@ -17,16 +17,50 @@ fn main() {
     // println!("instructions: {:?}", instructions);
 
     let mut polymer = start.to_string();
-    for _ in 0..10 {
+    for _ in 1..=10 {
         polymer = apply_instruction(&polymer, &instructions);
     }
-    // println!("polymer: {:?}", polymer);
+    // println!("polymer {}: {:?}", i, polymer);
+
     let mut keys = polymer.chars().counts().into_iter().sorted_by_key(|(_, count)| *count);
     let least = keys.next().unwrap().1;
     let most = keys.last().unwrap().1;
     println!("least: {}", least);
     println!("most: {}", most);
-    println!("difference: {}", most - least);
+    println!("part 1 - difference: {}", most - least);
+
+    // part2 - do it more clever without string appending
+    let mut pairs: HashMap<String, usize> = HashMap::new();
+    start.chars().tuple_windows::<(_, _)>().for_each(|(fst, snd)| {
+        let key = format!("{}{}", fst, snd);
+        *pairs.entry(key).or_insert(0) += 1;
+    });
+    // println!("pairs: {:?}", pairs);
+
+    for _ in 1..=40 {
+        let mut update = HashMap::new();
+        for (k, v) in pairs.iter() {
+            let key = format!("{}{}", k.chars().nth(0).unwrap(), instructions.get(k.as_str()).unwrap());
+            *update.entry(key).or_insert(0) += v;
+            let key = format!("{}{}", instructions.get(k.as_str()).unwrap(), k.chars().nth(1).unwrap());
+            *update.entry(key).or_insert(0) += v;
+        }
+        pairs = update;
+    }
+
+    // println!("pairs: {:?}", pairs);
+    let mut counts = HashMap::new();
+    for (k, v) in pairs.iter() {
+        let k: &str = k.as_ref();
+        *counts.entry(k.chars().nth(0).unwrap()).or_insert(0) += v;
+    }
+    *counts.entry(start.chars().last().unwrap()).or_insert(0) += 1;
+    let mut keys = counts.into_iter().sorted_by_key(|(_, count)| *count);
+    let least = keys.next().unwrap().1;
+    let most = keys.last().unwrap().1;
+    println!("least: {}", least);
+    println!("most: {}", most);
+    println!("part 2 - difference: {}", most - least);
 }
 
 fn apply_instruction(current: &str, instr: &HashMap<&str, &str>) -> String {
